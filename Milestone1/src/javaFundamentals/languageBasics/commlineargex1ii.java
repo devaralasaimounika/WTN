737eeffd1672a792aebfe98240d7161155c46236
SUMATHI MURALI REDDY <SUMATHI MURALI REDDY@DESKTOP-NQA5AHK>
2020-07-01 14:47:53 +0530

    Language Basics Problems

---
+++ Milestone1/src/javaFundamentals/languageBasics/commlineargex1ii.java
@@ -0,0 +1,8 @@
+package javaFundamentals.languageBasics;
+
+public class commlineargex1ii {
+public static void main(String args[])
+{
+	System.out.println(args[0]+" "+"Technologies"+" "+args[1]);
+}
+}
---
+++ Milestone1/src/javaFundamentals/languageBasics/commlineargex3.java
@@ -0,0 +1,11 @@
+package javaFundamentals.languageBasics;
+
+public class commlineargex3 {
+public static void main(String args[])
+{
+	int m=Integer.parseInt(args[0]);
+	int n=Integer.parseInt(args[1]);
+	int c=m+n;
+	System.out.println("The sum of "+m+" and "+n+" is" +c);
+}
+}
